5822ebfe28faac88e247b123f9dacf342e995905
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-09-23 23:57:50 -0300

    feat: cria controllers de cadastro de cliente

---
+++ meal4you_app/lib/controllers/client_register_controllers.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ClientRegisterControllers {
+  static final TextEditingController nomeController = TextEditingController();
+  static final TextEditingController emailController = TextEditingController();
+  static final TextEditingController senhaController = TextEditingController();
+  static final TextEditingController confirmarSenhaController =
+      TextEditingController();
+}
