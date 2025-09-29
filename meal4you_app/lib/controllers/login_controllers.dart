4df4fae5f55b5197f0e6edf806d089136660d4dc
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-09-29 03:01:22 -0300

    feat: cria controllers de login

---
+++ meal4you_app/lib/controllers/login_controllers.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class LoginControllers {
+  static final TextEditingController emailController = TextEditingController();
+  static final TextEditingController senhaController = TextEditingController();
+}
