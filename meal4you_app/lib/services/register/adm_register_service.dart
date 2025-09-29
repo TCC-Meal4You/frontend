18aaa1b35546dba2066708727fd1ed9599e592dd
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-09-29 01:55:36 -0300

    feat: cria service de cadastro de adm

---
+++ meal4you_app/lib/services/register/adm_register_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class AdmRegisterService {
+  static const String baseUrl =
+      "https://backend-production-6abd.up.railway.app/admin";
+
+  static Future<Map<String, dynamic>> registerAdm({
+    required String nome,
+    required String email,
+    required String senha,
+  }) async {
+    final url = Uri.parse(baseUrl);
+
+    final response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"nome": nome, "email": email, "senha": senha}),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception("Erro ao cadastrar: ${response.body}");
+    }
+  }
+}
