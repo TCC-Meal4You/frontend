faecfc096fc294f4b395a14916915bf4b986c67c
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-09-29 02:51:06 -0300

    feat: cria service login

---
+++ meal4you_app/lib/services/login/client_login_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ClientLoginService {
+  static const String baseUrl = "https://backend-production-6abd.up.railway.app/usuario/login";
+
+  static Future<Map<String, dynamic>> loginClient({
+    required String email,
+    required String senha,
+  }) async {
+    final url = Uri.parse(baseUrl);
+
+    final response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({
+        "email": email,
+        "senha": senha,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body) as Map<String, dynamic>;
+    } else {
+      throw Exception("Erro ao fazer login: ${response.body}");
+    }
+  }
+}
