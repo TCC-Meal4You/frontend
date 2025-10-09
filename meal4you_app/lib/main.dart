1ef85021523132b1f386b099547aa524f7d4198e
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-10-09 01:57:45 -0300

    feat: cria rotas

--- meal4you_app/lib/main.dart
+++ meal4you_app/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:meal4you_app/screens/change_data/change_adm_data_screen.dart';
+import 'package:meal4you_app/screens/change_data/change_client_data_screen.dart';
+import 'package:meal4you_app/screens/login/adm_login_screen.dart';
+import 'package:meal4you_app/screens/login/client_login_screen.dart';
+import 'package:meal4you_app/screens/profile/adm_profile_screen.dart';
+import 'package:meal4you_app/screens/profile/client_profile_screen.dart';
 import 'package:meal4you_app/screens/profile_choice/profile_choice_screen.dart';
+import 'package:meal4you_app/screens/register/adm_register_screen.dart';
+import 'package:meal4you_app/screens/register/client_register_screen.dart';
+import 'package:meal4you_app/screens/restrictions_choice/restrictions_choice_screen.dart';
+import 'package:meal4you_app/screens/time_choice/time_choice_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const Meal4You());
 }
 
@@ -13,7 +24,26 @@ class Meal4You extends StatelessWidget {
     return MaterialApp(
       title: 'Meal4You',
       debugShowCheckedModeBanner: false,
-      home: const ProfileChoiceScreen(),
+      initialRoute: '/profileChoice',
+      routes: {
+        '/changeAdmData': (context) => const ChangeAdmDataScreen(),
+        '/changeClientData': (context) => const ChangeClientDataScreen(),
+
+        '/admLogin': (context) => const AdmLoginScreen(),
+        '/clientLogin': (context) => const ClientLoginScreen(),
+
+        '/admProfile': (context) => const AdmProfileScreen(),
+        '/clientProfile': (context) => const ClientProfileScreen(),
+
+        '/profileChoice': (context) => const ProfileChoiceScreen(),
+
+        '/admRegister': (context) => const AdmRegisterScreen(),
+        '/clientRegister': (context) => const ClientRegisterScreen(),
+
+        '/restrictionsChoice': (context) => const RestrictionsChoiceScreen(),
+
+        '/timeChoice': (context) => const TimeChoiceScreen(),
+      },
     );
   }
 }
