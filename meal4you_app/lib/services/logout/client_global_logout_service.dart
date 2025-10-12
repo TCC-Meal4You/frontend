e750b3d4d9dd3d565f9d5f551b3801cc7cc44f40
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-10-12 16:25:18 -0300

    feat: feat: aplica logout global (deve ser arrumado)

---
+++ meal4you_app/lib/services/logout/client_global_logout_service.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'client_prefs.dart';
+
+class ClientGlobalLogoutService {
+  static const String _baseUrl =
+      "https://backend-backendmeal4you.up.railway.app/usuario";
+
+  final http.Client client;
+
+  ClientGlobalLogoutService({http.Client? client})
+    : client = client ?? http.Client();
+
+  Future<void> logoutGlobal() async {
+    final header = await ClientPrefs.getAuthorizationHeader();
+    final uri = Uri.parse('$_baseUrl/logout-global');
+
+    try {
+      final headers = <String, String>{'Content-Type': 'application/json'};
+      if (header != null) {
+        headers['Authorization'] = header;
+      }
+
+      final response = await client.post(uri, headers: headers);
+
+      if (response.statusCode == 200 || response.statusCode == 204) {
+        await ClientPrefs.clearAllUserData();
+        return;
+      } else {
+        throw HttpException(
+          'Falha ao tentar sair de todas as contas: '
+          '${response.statusCode} ${response.reasonPhrase}',
+        );
+      }
+    } on SocketException {
+      throw const SocketException('Sem conexão com a internet');
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
