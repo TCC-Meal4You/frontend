e1b3c2238c1d520f33cca2396bb14e06f5387d5c
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-10-12 17:53:02 -0300

    feat: cria logout global do adm (deve ser arrumado)

---
+++ meal4you_app/lib/services/logout/adm_logout/adm_global_logout_service.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:meal4you_app/services/logout/adm_logout/adm_prefs.dart';
+
+class AdmGlobalLogoutService {
+  static const String _baseUrl =
+      "https://backend-backendmeal4you.up.railway.app/admin";
+
+  final http.Client adm;
+
+  AdmGlobalLogoutService({http.Client? adm})
+    : adm = adm ?? http.Client();
+
+  Future<void> logoutGlobal() async {
+    final header = await AdmPrefs.getAuthorizationHeader();
+    final uri = Uri.parse('$_baseUrl/logout-global');
+
+    try {
+      final headers = <String, String>{'Content-Type': 'application/json'};
+      if (header != null) {
+        headers['Authorization'] = header;
+      }
+
+      final response = await adm.post(uri, headers: headers);
+
+      if (response.statusCode == 200 || response.statusCode == 204) {
+        await AdmPrefs.clearAllUserData();
+        return;
+      } else {
+        throw HttpException(
+          'Falha ao tentar sair de todas as contas: '
+          '${response.statusCode} ${response.reasonPhrase}',
+        );
+      }
+    } on SocketException {
+      throw const SocketException('Sem conexão com a internet');
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
