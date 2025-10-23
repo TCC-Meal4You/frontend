353f7edde62f530e0d9b59f07855dce86612ac8e
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-10-23 20:45:35 -0300

    controller: cria controllers dos campos de restaurante

---
+++ meal4you_app/lib/controllers/restaurant/restaurant_controllers.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class RestaurantControllers {
+  static final TextEditingController nameController = TextEditingController();
+  static final TextEditingController descriptionController = TextEditingController();
+}
