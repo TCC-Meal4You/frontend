9d67bd2fd9b314630e7f7103fc5f456f012645bf
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-11-01 20:01:24 -0300

    refactor: atualiza service de cadastro

--- meal4you_app/lib/services/register/client_register_service.dart
+++ meal4you_app/lib/services/register/client_register_service.dart
@@ -9,20 +9,30 @@ class ClientRegisterService {
     required String nome,
     required String email,
     required String senha,
+    required String codigo,
   }) async {
     final url = Uri.parse(baseUrl);
+
+    final body = jsonEncode({
+      "nome": nome,
+      "email": email,
+      "senha": senha,
+      "codigoVerificacao": codigo,
+    });
+
 
     final response = await http.post(
       url,
       headers: {"Content-Type": "application/json"},
-      body: jsonEncode({"nome": nome, "email": email, "senha": senha}),
+      body: body,
     );
+
 
     if (response.statusCode == 200 || response.statusCode == 201) {
       return jsonDecode(response.body);
     } else {
       throw Exception(
-        "Erro ao cadastrar: ${response.statusCode} - ${response.body}",
+        "Erro ao cadastrar cliente: ${response.statusCode} - ${response.body}",
       );
     }
   }
