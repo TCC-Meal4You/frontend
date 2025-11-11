9297fa193cfd905bdfcf3c8a46662ca8b4ff819f
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-11-11 05:32:10 -0300

    feat: adiciona id

--- meal4you_app/lib/provider/restaurant_provider.dart
+++ meal4you_app/lib/provider/restaurant_provider.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 class RestaurantProvider extends ChangeNotifier {
+  int? _id;
   String _name = '';
   String _description = '';
   String _location = '';
   bool _isActive = false;
   List<String> _foodTypes = [];
 
-  // Getters
+  int? get id => _id;
   String get name => _name;
   String get description => _description;
   String get location => _location;
@@ -15,12 +16,14 @@ class RestaurantProvider extends ChangeNotifier {
   List<String> get foodTypes => _foodTypes;
 
   void updateRestaurant({
+    required int id,
     required String name,
     required String description,
     required String location,
     required bool isActive,
     required List<String> foodTypes,
   }) {
+    _id = id;
     _name = name;
     _description = description;
     _location = location;
@@ -28,6 +31,12 @@ class RestaurantProvider extends ChangeNotifier {
     _foodTypes = foodTypes;
     notifyListeners();
   }
+
+  void updateId(int id) {
+    _id = id;
+    notifyListeners();
+  }
+
   void updateName(String name) {
     _name = name;
     notifyListeners();
