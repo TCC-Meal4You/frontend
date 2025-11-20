80d77354fde16d99b6c5b292fbc8eb5a8e515165
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-11-19 23:04:54 -0300

    fix: remove localizacao

--- meal4you_app/lib/providers/restaurant_provider.dart
+++ meal4you_app/lib/providers/restaurant_provider.dart
@@ -4,14 +4,12 @@ class RestaurantProvider extends ChangeNotifier {
   int? _id;
   String _name = '';
   String _description = '';
-  String _location = '';
   bool _isActive = false;
   List<String> _foodTypes = [];
 
   int? get id => _id;
   String get name => _name;
   String get description => _description;
-  String get location => _location;
   bool get isActive => _isActive;
   List<String> get foodTypes => _foodTypes;
 
@@ -19,14 +17,12 @@ class RestaurantProvider extends ChangeNotifier {
     required int id,
     required String name,
     required String description,
-    required String location,
     required bool isActive,
     required List<String> foodTypes,
   }) {
     _id = id;
     _name = name;
     _description = description;
-    _location = location;
     _isActive = isActive;
     _foodTypes = foodTypes;
     notifyListeners();
@@ -47,11 +43,6 @@ class RestaurantProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void updateLocation(String location) {
-    _location = location;
-    notifyListeners();
-  }
-
   void updateIsActive(bool isActive) {
     _isActive = isActive;
     notifyListeners();
@@ -66,7 +57,6 @@ class RestaurantProvider extends ChangeNotifier {
   _id = null;
   _name = '';
   _description = '';
-  _location = '';
   _isActive = false;
   _foodTypes = [];
   notifyListeners();
@@ -76,7 +66,6 @@ void resetRestaurant() {
   _id = null;
   _name = '';
   _description = '';
-  _location = '';
   _isActive = false;
   _foodTypes = [];
   notifyListeners();
