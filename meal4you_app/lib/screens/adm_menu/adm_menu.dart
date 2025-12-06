120dd1b176873602c175821ebdef72fb134cb3ef
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-12-06 14:12:08 -0300

    feat: adiciona animação de transição

--- meal4you_app/lib/screens/adm_menu/adm_menu.dart
+++ meal4you_app/lib/screens/adm_menu/adm_menu.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:meal4you_app/models/ingredient_response_dto.dart';
 import 'package:meal4you_app/models/meal_request_dto.dart';
 import 'package:meal4you_app/models/meal_response_dto.dart';
+import 'package:meal4you_app/screens/home/adm_restaurant_home_screen.dart';
+import 'package:meal4you_app/screens/profile/adm_profile_screen.dart';
 import 'package:meal4you_app/services/ingredient/ingredient_service.dart';
 import 'package:meal4you_app/services/meal/meal_service.dart';
 
@@ -115,11 +117,57 @@ class _AdmMenuScreenState extends State<AdmMenuScreen> {
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.person_outline),
-              label: 'Meu Perfil',
+              label: 'Perfil',
             ),
           ],
           currentIndex: 0,
-          onTap: (index) {},
+          onTap: (index) {
+            if (index == 0) {
+              Navigator.of(context).pushReplacement(
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) {
+                    return const AdmRestaurantHomeScreen();
+                  },
+                  transitionsBuilder:
+                      (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(-1.0, 0.0);
+                        const end = Offset.zero;
+                        const curve = Curves.easeInOut;
+                        var tween = Tween(
+                          begin: begin,
+                          end: end,
+                        ).chain(CurveTween(curve: curve));
+                        return SlideTransition(
+                          position: animation.drive(tween),
+                          child: child,
+                        );
+                      },
+                ),
+              );
+            } else if (index == 1) {
+              Navigator.of(context).push(
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) {
+                    return const AdmProfileScreen();
+                  },
+                  transitionsBuilder:
+                      (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(1.0, 0.0);
+                        const end = Offset.zero;
+                        const curve = Curves.easeInOut;
+                        var tween = Tween(
+                          begin: begin,
+                          end: end,
+                        ).chain(CurveTween(curve: curve));
+                        return SlideTransition(
+                          position: animation.drive(tween),
+                          child: child,
+                        );
+                      },
+                ),
+              );
+            }
+          },
         ),
       ),
     );
