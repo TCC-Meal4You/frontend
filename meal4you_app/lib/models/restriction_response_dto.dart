e2b87bdedaa1a34a19f6d5049ce8db0586c9b810
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-12-06 13:33:39 -0300

    feat: cria dto de resposta para restrições

---
+++ meal4you_app/lib/models/restriction_response_dto.dart
@@ -0,0 +1,13 @@
+class RestrictionResponseDTO {
+  final int idRestricao;
+  final String nome;
+
+  RestrictionResponseDTO({required this.idRestricao, required this.nome});
+
+  factory RestrictionResponseDTO.fromJson(Map<String, dynamic> json) {
+    return RestrictionResponseDTO(
+      idRestricao: json['idRestricao'] ?? json['id_restricao'] ?? 0,
+      nome: json['nome'] ?? '',
+    );
+  }
+}
