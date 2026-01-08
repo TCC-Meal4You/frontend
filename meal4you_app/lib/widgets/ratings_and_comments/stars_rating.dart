48ce78d94c43d575d49ab0d2146e717a4e2ae789
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2026-01-07 23:34:43 -0300

    feat: cria avaliação de estrelas

---
+++ meal4you_app/lib/widgets/ratings_and_comments/stars_rating.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class StarsRating extends StatelessWidget {
+  final double rating;
+
+  const StarsRating({super.key, required this.rating});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(5, (index) {
+        return Icon(
+          index < rating ? Icons.star : Icons.star_border,
+          color: Colors.amber,
+          size: 20,
+        );
+      }),
+    );
+  }
+}
