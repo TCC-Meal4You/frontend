e283f3eb2709286156651f02fe3337c77319d502
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2026-02-19 16:46:25 -0300

    feat: atualiza tela de home

--- meal4you_app/lib/screens/home/client_home_screen.dart
+++ meal4you_app/lib/screens/home/client_home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:meal4you_app/controllers/logout_handlers/client_logout_handler.dart';
 import 'package:meal4you_app/screens/search_restaurant_and_dish/search_restaurant_and_dish_screen.dart';
 
 class ClientHomeScreen extends StatefulWidget {
@@ -9,58 +11,137 @@ class ClientHomeScreen extends StatefulWidget {
 }
 
 class _ClientHomeScreenState extends State<ClientHomeScreen> {
+  final ClientLogoutHandler _clientlogoutHandler = ClientLogoutHandler();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home do Cliente'),
-      ),
-      body: const Center(
-        child: Text('Bem-vindo à Home do Cliente!'),
-      ),
-       bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: Colors.white,
-            selectedItemColor: const Color.fromARGB(255, 157, 0, 255),
-            unselectedItemColor: Colors.grey,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home_rounded),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'Buscar',
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xFFFCF9FF),
+        resizeToAvoidBottomInset: true,
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.only(bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 190,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromARGB(255, 157, 0, 255),
+                      Color.fromARGB(255, 15, 230, 135),
+                    ],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 16,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'MEAL4YOU',
+                                style: TextStyle(
+                                  fontFamily: 'Ubuntu',
+                                  fontSize: 27,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                'c  o  m  i  d  a    c  o  n  s  c  i  e  n  t  e',
+                                style: TextStyle(
+                                  fontFamily: 'Ubuntu',
+                                  fontSize: 8,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          IconButton(
+                            onPressed: () =>
+                                _clientlogoutHandler.showLogoutDialog(context),
+                            icon: const FaIcon(
+                              FontAwesomeIcons.rightFromBracket,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Bem-vindo!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontFamily: 'Ubuntu',
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Buscando opções para: ',
+                      style: const TextStyle(
+                        color: Colors.white70,
+                        fontSize: 15,
+                        fontFamily: 'Ubuntu',
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
-            currentIndex: 0,
-            onTap: (index) {
-              if (index == 0) {
-                Navigator.pushReplacementNamed(context, '/searchRestaurantAndDish');
-              } else if (index == 1) {
-                Navigator.of(context).push(
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation, secondaryAnimation) {
-                      return const SearchRestaurantAndDishScreen();
-                    },
-                    transitionsBuilder:
-                        (context, animation, secondaryAnimation, child) {
-                          const begin = Offset(1.0, 0.0);
-                          const end = Offset.zero;
-                          const curve = Curves.easeInOut;
-                          var tween = Tween(
-                            begin: begin,
-                            end: end,
-                          ).chain(CurveTween(curve: curve));
-                          return SlideTransition(
-                            position: animation.drive(tween),
-                            child: child,
-                          );
-                        },
-                  ),
-                );
-              }
-            },
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          selectedItemColor: const Color.fromARGB(255, 157, 0, 255),
+          unselectedItemColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Busca'),
+          ],
+          currentIndex: 0,
+          onTap: (index) {
+            if (index == 0) {
+              Navigator.pushReplacementNamed(context, '/clientHome');
+            } else if (index == 1) {
+              Navigator.of(context).push(
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) {
+                    return const SearchRestaurantAndDishScreen();
+                  },
+                  transitionsBuilder:
+                      (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(1.0, 0.0);
+                        const end = Offset.zero;
+                        const curve = Curves.easeInOut;
+                        var tween = Tween(
+                          begin: begin,
+                          end: end,
+                        ).chain(CurveTween(curve: curve));
+                        return SlideTransition(
+                          position: animation.drive(tween),
+                          child: child,
+                        );
+                      },
+                ),
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
