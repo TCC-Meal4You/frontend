5a737d063f934ad01316ffac505b7bb75ae3a69c
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2026-05-07 05:17:54 -0300

    .

--- meal4you_app/lib/utils/formatter/date_formatter.dart
+++ meal4you_app/lib/utils/formatter/date_formatter.dart
@@ -1,6 +1,11 @@
 String formatDate(DateTime date) {
   final now = DateTime.now();
-  final difference = now.difference(date);
+  final localDate = date.toLocal();
+  final difference = now.difference(localDate);
+
+  if (difference.isNegative) {
+    return 'Agora';
+  }
 
   if (difference.inDays == 0) {
     if (difference.inHours == 0) {
@@ -15,6 +20,20 @@ String formatDate(DateTime date) {
   } else if (difference.inDays < 7) {
     return '${difference.inDays}d atrás';
   } else {
-    return '${date.day}/${date.month}/${date.year}';
+    return '${localDate.day.toString().padLeft(2, '0')}/${localDate.month.toString().padLeft(2, '0')}/${localDate.year}';
   }
+}
+
+String formatExactDateTime(DateTime date) {
+  final localDate = date.toLocal();
+  return '${localDate.day.toString().padLeft(2, '0')}/${localDate.month.toString().padLeft(2, '0')}/${localDate.year} '
+      '${localDate.hour.toString().padLeft(2, '0')}:${localDate.minute.toString().padLeft(2, '0')}';
+}
+
+String formatExactDateOrDateOnly(DateTime date, {bool? hasTime}) {
+  final localDate = date.toLocal();
+  final datePart = '${localDate.day.toString().padLeft(2, '0')}/${localDate.month.toString().padLeft(2, '0')}/${localDate.year}';
+  final showTime = hasTime ?? true;
+  if (!showTime) return datePart;
+  return '$datePart ${localDate.hour.toString().padLeft(2, '0')}:${localDate.minute.toString().padLeft(2, '0')}';
 }
